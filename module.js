1c4495e2e8ff794ca2d347a677d4aac73a809769
Ludovic Araye <34149197+landacus@users.noreply.github.com>
2026-02-23 09:01:11 +0100

    Revert modify

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 import * as duckdb from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/+esm';
-import { renderRegionalMap } from 'init.js';
-import { renderDepartmentMap } from 'dep.js';
+import { renderRegionalMap } from './init.js';
+import { renderDepartmentMap } from './dep.js';
 
 let conn;
 let posLevel = "region"; // "region" ou "departement"
@@ -30,7 +30,7 @@ async function setup() {
 
     // Charger le fichier Parquet via fetch
     console.log("Chargement du fichier data.parquet...");
-    const response = await fetch('data.parquet');
+    const response = await fetch('./data.parquet');
     const arrayBuffer = await response.arrayBuffer();
     await db.registerFileBuffer('data.parquet', new Uint8Array(arrayBuffer));
 
